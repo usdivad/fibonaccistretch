8ee8973a59a07bfe91623e07372f0bc1643611ae
David Su <david.d.su@gmail.com>
2017-05-09 00:47:48 +0900

    import implementation of bjorklund's algorithm

---
+++ bjorklund.py
@@ -0,0 +1,40 @@
+"""
+Copyright (c) 2011 Brian House
+https://github.com/brianhouse/bjorklund
+"""
+
+def bjorklund(steps, pulses):
+    steps = int(steps)
+    pulses = int(pulses)
+    if pulses > steps:
+        raise ValueError    
+    pattern = []    
+    counts = []
+    remainders = []
+    divisor = steps - pulses
+    remainders.append(pulses)
+    level = 0
+    while True:
+        counts.append(divisor / remainders[level])
+        remainders.append(divisor % remainders[level])
+        divisor = remainders[level]
+        level = level + 1
+        if remainders[level] <= 1:
+            break
+    counts.append(divisor)
+    
+    def build(level):
+        if level == -1:
+            pattern.append(0)
+        elif level == -2:
+            pattern.append(1)         
+        else:
+            for i in xrange(0, counts[level]):
+                build(level - 1)
+            if remainders[level] != 0:
+                build(level - 2)
+    
+    build(level)
+    i = pattern.index(1)
+    pattern = pattern[i:] + pattern[0:i]
+    return pattern
