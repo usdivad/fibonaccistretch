1560faf62834bf7bd67573fee5b81d3fdafd9cc5
David Su <david.d.su@gmail.com>
2017-05-23 12:15:26 -0700

    add script to execute notebooks and update git; scrap accidental euclid cell

---
+++ execute_notebooks.sh
@@ -0,0 +1,28 @@
+# Script to execute Jupyter notebooks and save output
+
+# Args
+# commit_msg=$1
+
+# Create temporary copies of notebooks
+cp fibonaccistretch.ipynb tmp_fibonaccistretch_executed.ipynb
+cp export_with_figs.ipynb tmp_export_with_figs_executed.ipynb
+cp link_ipd_audio.ipynb tmp_link_ipd_audio_executed.ipynb
+
+# Remove existing figures
+rm -rf data/figs
+mkdir data/figs
+
+# Run the notebooks
+jupyter nbconvert --to notebook --execute tmp_fibonaccistretch_executed.ipynb
+jupyter nbconvert --to notebook --execute tmp_export_with_figs_executed.ipynb
+jupyter nbconvert --to notebook --execute tmp_link_ipd_audio_executed.ipynb
+
+# Remove all temporary files
+rm tmp_*.ipynb
+# rm tmp_fibonaccistretch_executed.ipynb
+# rm tmp_export_with_figs_executed.ipynb
+# rm tmp_link_ipd_audio_executed.ipynb
+
+git add -u
+git add data/figs
+# git commit -m "$1"
